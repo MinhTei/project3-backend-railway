e9efc4d2ab7f9f8e743835b9beb5bdfb04305cb9
Minh Tai <buiminhtai97@example.com>
2025-12-14 00:52:23 +0700

    test flatform

--- index.js
+++ index.js
@@ -3,8 +3,7 @@ const app = express()
 const port = process.env.PORT || 3000 // Render sẽ tự điền cổng vào đây
 
 app.get('/', (req, res) => {
-  res.send('<h1>Chao mung den voi Cloud Platform (Render) 🚀</h1><p>Trang web nay duoc Deploy tu dong!</p>')
-     res.send('<p>Ban co the tham khao them ve Render tai <a href="https://render.com/docs">Render Documentation</a></p>')
+  res.send('<h1>Chao mung den voi Cloud Platform (Render) 🚀</h1><p>Trang web nay duoc Deploy tu dong!</p><h2>Minh tài đã thành công Flatform </h2>')
 })
 
 app.listen(port, () => {
